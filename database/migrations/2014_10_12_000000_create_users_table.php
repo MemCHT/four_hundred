14128c48583c1d195bd573b540f00661f5608506
syamazaki <syamazaki1678@gmail.com>
2020-09-25 18:01:12 +0900

    プロフィール編集 リファクタ前

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('password',255);
             $table->rememberToken();
             $table->tinyInteger('status_id')->unsigned()->default(1);   //公開（正常）
-            $table->string('icon',255)->default('default.jpg');
+            $table->string('icon',255)->default('default.png');
             $table->dateTime('created_at');
             $table->dateTime('updated_at');
 
